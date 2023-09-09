210300528746110b8f33ce3bfc46e6182ad66bc4
mendjijet <mendjijet@gmail.com>
2023-09-09 07:54:34 +0100

    first commit

---
+++ src/main/java/com/jet/QrcodeEncrypted/QrCodeEncrypt/QrCodeEncryptApplication.java
@@ -0,0 +1,13 @@
+package com.jet.QrcodeEncrypted.QrCodeEncrypt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QrCodeEncryptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QrCodeEncryptApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jet/QrcodeEncrypted/QrCodeEncrypt/QrCodeEncryptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jet.QrcodeEncrypted.QrCodeEncrypt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QrCodeEncryptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
